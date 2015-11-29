123429030f2776ac1db5fdbad3a7f05269bd477b
Laura Santos <lbsantosg@unal.edu.co>
2015-11-29 15:12:24 -0500

    Adding the drop

--- Games/colors.js
+++ Games/colors.js
@@ -1,24 +1,22 @@
 var Colors = function ()
 {
 	var fst_cube , color_Cube, cubes, drop;
-	var gameOver = true;
+	var GameOver = true;
 	this.setup = function()
 	{
 		createCanvas(600,400);
 		colorMode(HSB, 360, 100, 100);
-		background(200,50,100);
 		//Create a group of sprites "cubes" with different colors 
 		//that will be the target of the drop.
 		cubes= new Group();
 		fst_cube = width / 10; 
 		for ( var i = 0 ; i < 5 ; i ++ )	
 		{
-			color_Cube = createSprite( fst_cube, height-50, width/5 , 100); 
+			color_Cube = createSprite( fst_cube, 500, width/5 , 50); 
 			color_Cube.shapeColor =  addColor(i); 
 			fst_cube += width / 5;
 			cubes.add(color_Cube);
 		}	
-		drawSprites(cubes);		
 	};
 	
 	addColor= function(n)
@@ -50,16 +48,26 @@ var Colors = function ()
 // 
  	draw = function()
  	{
-		if ( !GameOver )
-		{
-			drop = createSprite( width /2 , height /2 , 100 , 100); 
-		}
-		drawSprite(drop);
+ 		background(200,50,100);
+ 		drawSprites(cubes);	
+ 		if ( ! GameOver )
+ 		{
+ 			drawSprite(drop);
+ 			drop.velocity.y = 1.0;
+ 		    drop.position.x = constrain(mouseX, 0, width);	
+ 		}
+
+ 	};
+ 	NewGame = function ()
+ 	{
+ 			drop = createSprite( width /2 , 100 , 20 , 20);
+ 			drop.shapeColor = addColor( random (0,4) ); 
  	};
     mousePressed = function()
    {
   		if(GameOver)
     		GameOver= false;
+    	NewGame();
 	}
 
 };
