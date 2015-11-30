28aa2df6c43f329c55aa2cb747604bbc6a692b49
Jimmy Pulido <jiapulidoar@gmail.com>
2015-11-29 21:25:08 -0500

    Add visuals Galaga

--- Games/galaga.js
+++ Games/galaga.js
@@ -6,20 +6,23 @@ var Galaga = function ()
 	var psize = 40, GameOver;
 	var asteroids, Count, level = 0; 
 	var bulls ;
-	var frame = 10;
+	var frame = 1;
 	var ROWS = 5 , COL = 12, total;
-	var bgImg;
+	var bgImg, planeimg;
 	
 	this.setup = function()
 	{
 
 		frameRate(frame);
-		bgImg = loadImage("images/flappy_bg.png");
+		bgImg = loadAnimation("images/Galaga/back1","images/Galaga/back2","images/Galaga/back3","images/Galaga/back4");
+		planeimg = loadImage("images/Galaga/Galaga.png");
+		bgImg.frameDelay = 10;
 		GameOver = true;
 		asteroids = new Group(); 
 		bulls =  new Group(); 
 		Plane = createSprite(width/2, height/1.2, psize,psize);
-  		Plane.draw = function(){triangle(psize/2,0,0,psize,psize,psize) }
+		Plane.addImage(planeimg);
+  		//Plane.draw = function(){triangle(psize/2,0,0,psize,psize,psize) }
   		textSize(50);
   		text("Click New Game",width/2,height/2);
 
@@ -30,7 +33,7 @@ var Galaga = function ()
 		//clear();
 		textSize(30);
 		if(!GameOver){
-			background(160,50,80);
+			background(0);
 			text ( Count, width-40 , 30)
 			Plane.position.x = constrain(mouseX-psize/2,0,width-psize);
 
@@ -48,8 +51,8 @@ var Galaga = function ()
 			if(Count >= total)
 				win();
 
-			image( bgImg, 0,height/2);
-			image(bgImg, 400, height/2)
+			animation( bgImg, width/2,height/2);
+
 			camera.on();
 			asteroids.overlap(bulls,erase);
 			drawSprites();
@@ -86,7 +89,7 @@ var Galaga = function ()
 	mousePressed = function() 
 	{
 		if(bulls.length<3){
-			var s = createSprite(constrain(mouseX,psize/2,width-psize/2),height/1.2, 5,10);
+			var s = createSprite(constrain(mouseX-psize/2,0,width-psize),height/1.2 - 10, 5,10);
 			bulls.add(s);
 			s.velocity.y =  -4;
 			s.life=height/1.5;
@@ -115,13 +118,18 @@ var Galaga = function ()
 	  				asteroids.add(a);
 	  				a.velocity.y=0.2;
 	  				total++;
+	  				a.addAnimation("hla","images/Galaga/gbee1.png", "images/Galaga/gbee2.png");
+	  				a.animation.frameDelay = 50;
+	  
 	  			}
 	  			for(var j=ROWS-i-1; j<COL-(ROWS-i-1); j++)
 	  			{
 	  				var a = createSprite( 25*(j) + 25 *COL,25*(i+1), 20,20);
 	  				asteroids.add(a);
-	  				a.velocity.y=0.1;
+	  				a.velocity.y=0.2;
 	  				total++;
+	  				a.addAnimation("hola","images/Galaga/bbee1.png", "images/Galaga/bbee2.png");
+	  				a.animation.frameDelay = 50;
 	  			}
 	  		}
 	  		break;
